ae570139d30027ed89c47ae8be4355fb0224f6a5
root <root@perf109-51.qa.lab>
2020-03-27 13:23:40 -0700

    fix for MKE

--- SVM/src/main/scala/SVMDataGen.scala
+++ SVM/src/main/scala/SVMDataGen.scala
@@ -25,6 +25,7 @@ import scala.util.Random
 import com.github.fommil.netlib.BLAS.{getInstance => blas}
 
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 import org.apache.spark.annotation.{DeveloperApi, Since}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
@@ -35,19 +36,20 @@ object SVMDataGen {
         def main(args: Array[String]) {
             if (args.length < 2) {
                 println("Usage: SVMGenerator " +
-                        "<master> <output_dir> [num_examples] [num_features] [num_partitions]")
+                        "<Name> <output_dir> [num_examples] [num_features] [num_partitions]")
                     System.exit(1)
             }
 	Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
-            val sparkMaster: String = args(0)
+                val sparkMaster: String = args(0)
                 val outputPath: String = args(1)
                 val nexamples: Int = if (args.length > 2) args(2).toInt else 1000
                 val nfeatures: Int = if (args.length > 3) args(3).toInt else 2
                 val parts: Int = if (args.length > 4) args(4).toInt else 2
 
-                val sc = new SparkContext(sparkMaster, "SVMGenerator")
+                val conf = new SparkConf().setAppName("SVMGenerator")
+                val sc = new SparkContext(conf)
 
                 val globalRnd = new Random(94720)
                 //val trueWeights = Array.fill[Double](nfeatures + 1)(globalRnd.nextGaussian())
